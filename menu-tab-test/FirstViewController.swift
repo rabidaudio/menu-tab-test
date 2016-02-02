7d2b1d641266371e4dac1b2eeefc639bf375dc3d
Charles Julian Knight <charles@rabidaudio.com>
2016-02-01 23:15:44 -0500

    Initial Commit

---
+++ menu-tab-test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  menu-tab-test
+//
+//  Created by fixd on 2/1/16.
+//  Copyright © 2016 fixd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
