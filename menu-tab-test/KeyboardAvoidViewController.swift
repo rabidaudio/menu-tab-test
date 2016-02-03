ad6b2baada807f96a1413678423cedae29c3805d
Charles Julian Knight <charles@rabidaudio.com>
2016-02-03 11:58:13 -0500

    cleaned up keyboardavoid

--- menu-tab-test/KeyboardAvoidViewController.swift
+++ menu-tab-test/KeyboardAvoidViewController.swift
@@ -2,12 +2,16 @@
 //  KeyboardAvoidViewController.swift
 //  menu-tab-test
 //
-//  Created by fixd on 2/3/16.
-//  Copyright © 2016 fixd. All rights reserved.
+//  Created by @charlesjuliank on 2/3/16.
 //
 
 import UIKit
 
+// This is a pretty easy to use ViewController for allowing views to move into screen when the keyboard shows.
+// Simply set your view controller class to this or a subclass, and set the class of the top-level view of
+// the controller to a UIScrollView. No other changes are necessary. This will slide the currently focused
+// view to be in the center of the remaining screen space. It handles changing focus and auto-closing the keyboard
+// when leaving focus. It also correctly handles hardware keyboards like in the emulator (that is, it does nothing).
 class KeyboardAvoidViewController: UIViewController {
     
     var scrollView: UIScrollView {
@@ -20,8 +24,7 @@ class KeyboardAvoidViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //double height for keyboard should be more than enough
-//        scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: 2*scrollView.bounds.height)
+        // make scrollView uninteractable
         scrollView.scrollEnabled = false
         scrollView.pagingEnabled = false
         scrollView.bounces = false
@@ -30,18 +33,16 @@ class KeyboardAvoidViewController: UIViewController {
         scrollView.minimumZoomScale = 1
         scrollView.maximumZoomScale = 1
         
+        // listen for taps to close keyboard
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "tap"))
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidShow:", name: UIKeyboardDidShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func keyboardWillShow(notification: NSNotification) {
         if let keyboardHeight = notification.userInfo?[UIKeyboardFrameEndUserInfoKey]?.CGRectValue.size.height {
-            print("keyboard height", keyboardHeight, "view height", view.bounds.height, keyboardHeight/view.bounds.height)
             scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: scrollView.bounds.height + keyboardHeight)
-//            let newRect = CGRect(origin: CGPoint(x: 0, y: keyboardHeight/2), size: scrollView.bounds.size)
             if let centerView = selectedView {
                 // the new origin to scroll to is the one which centers the selected view in the space above the keyboard
                 // to find this offset, we take the y position of the view and subtract the y position of the new center point
@@ -54,21 +55,17 @@ class KeyboardAvoidViewController: UIViewController {
         }
     }
     
-    func keyboardDidShow(notification: NSNotification) {
-        print("new bounds", scrollView.bounds)
-    }
-    
     func keyboardWillHide(notification: NSNotification) {
-        print("will hide", scrollView.bounds)
+        // reset by chaning the scrollView's content size back to normal
         scrollView.contentSize = scrollView.bounds.size
-        scrollView.scrollRectToVisible(CGRect(origin: CGPoint(x: 0, y: 0), size: view.bounds.size), animated: true)
+        scrollView.scrollRectToVisible(scrollView.bounds, animated: true)
     }
     
     func tap() {
-        //resign first responder so the keyboard will close
-        selectedView?.resignFirstResponder()
+        selectedView?.resignFirstResponder() //resign first responder so the keyboard will close
     }
     
+    // find the currently selectedview
     private var selectedView: UIView? {
         for subview in view.subviews {
             if subview.isFirstResponder() {
